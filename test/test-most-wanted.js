c2be81605930d826424e722f94511002d2df3140
malinkie <scott.morrice@gmail.com>
2013-10-07 14:15:24 -0700

    Adding some test cases for most wanted functionality...

---
+++ test/test-most-wanted.js
@@ -0,0 +1,67 @@
+var qconf = require('../qconf.js'),
+    test = require('tape');
+
+/**
+ * If a function is passed as the only parameter, qconf
+ * loads sources asyncronously.
+**/
+test('async support - only callback', function (t) {
+  qconf.clear();
+  qconf(function(err, config){
+    t.error(err, 'should not cause error');
+    t.ok(config, 'should return config singleton');
+    t.end();
+  });
+});
+
+/**
+ * If a function is passed as the last parameter, qconf
+ * loads sources asyncronously.
+**/
+test('async support - with default file override', function (t) {
+  qconf.clear();
+  qconf("config/config.json", function(err, config){
+    t.error(err, 'should not cause error');
+    t.ok(config, 'should return config singleton');
+    t.end();
+  });
+});
+
+/**
+ * If a function is passed as the last parameter, qconf
+ * loads sources asyncronously.
+**/
+test('async support - with filelist and override', function (t) {
+  qconf.clear();
+  qconf({hello: 'world'},["config/config.json"], function(err, config){
+    t.error(err, 'should not cause error');
+    t.ok(config, 'should return config singleton');
+    t.end();
+  });
+});
+
+/**
+ * Add support for HTTP(S) configuration sources
+ * Keep it simple at first:
+ *   GET requests only
+ *   JSON responses only
+ * Forces the requirment of a callback, therefore forcing qconf initialization to asyncronous.
+**/
+test('http', function (t) {
+  qconf.clear();
+  qconf("http://www.getyourjsonconfighere.com", function(err, config){
+    t.error(err, 'should not cause error');
+    t.ok(config, 'should return config singleton');
+    t.end();
+  });
+});
+
+/**
+ * Add support for HTTP(S) configuration sources
+ * Error thrown in the case of not passing qconf a callback function
+ * when asking for an http(s) source
+**/
+test('http - no callback', function (t) {
+  qconf.clear();
+  t.throws(qconf("http://www.getyourjsonconfighere.com"), "Some message", "should throw an error due to missing callback");
+});
